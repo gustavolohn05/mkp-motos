07ad2e12a67d92a3cc4ffaedc127f1c3e56a5449
mpkmotos <mpkmotos@gmail.com>
2026-05-12 23:05:06 -0300

    debug supabase env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,4 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 const url = (import.meta as any).env.VITE_SUPABASE_URL
 const key = (import.meta as any).env.VITE_SUPABASE_ANON_KEY
 
+console.log('Supabase URL:', url)
+console.log('Supabase KEY:', key ? 'definida' : 'indefinida')
+
 export const supabase = createClient(url, key)
